f5aee027446173b7c1a05161098f72de7bc50106
qwe-kev <119116884+qwe-kev@users.noreply.github.com>
2022-11-29 22:49:45 +0530

    DOM task 7

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-var items = document.querySelectorAll(".list-group-item");
-items[1].style.backgroundColor = "green";
-items[2].style.display="none";
-items[1].style.color = "green";
+var itemList = document.querySelector("#items");
 
-for(let i = 0;i < items.length;i++) {
-    if(!i%2==0) {
-        items[i].style.backgroundColor = "green";
-    }
-}
+//parentNode parentElement
+itemList.parentNode.style.backgroundColor = "#f4f4f4"
+
+//children
+itemList.children[1].style.fontFamily = "Verdana";
+
+//firstElementChild
+itemList.firstElementChild.style.color = "blue";
+
+//lastElementChild
+itemList.lastElementChild.textContent = "Hello 4";
+
+//nextSibling
+itemList.nextSibling = document.createElement('div')
+
+//nextElementSibling
+console.log(itemList.nextElementSibling);
+
+//previousSibling
+itemList.previousSibling;
+
+//previousElementSibling
+itemList.previousElementSibling
+
+// createElement
+const newDiv = document.createElement('div');
+
+// add class
+newDiv.className = "NEW DIV";
+
+// add id
+newDiv.id = "DIV1"
+
+// add attribut
+newDiv.setAttribute('title','NEW ELEMENT');
+
+//add textNode
+
+var divContent = document.createTextNode("Hello World");
+newDiv.appendChild(divContent);
+
+let container = document.querySelector("header .container");
+let header = document.querySelector("header h1");
+
+
+container.insertBefore(newDiv, header);
+itemList.children.insertBefore(newDiv, itemList[0])
