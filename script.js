6c9ede3dd188f289ed8ac9eebc2b198ce9ddc386
qwe-kev <119116884+qwe-kev@users.noreply.github.com>
2022-12-03 11:21:10 +0530

    DOM task 9

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-form.addEventListener('submit',addItem);
-itemList.addEventListener('click', removeItem);
+var itemList = document.getElementById("items");
+var formInput = document.getElementById("addForm");
+var filter = document.getElementById("filter");
 
-function addItem(e){
+var addItem = function(e) {
     e.preventDefault();
-    var li = document.createElement('li');
     var itemValue = document.getElementById("item").value;
+    var description = document.getElementById("item-desc").value;
+    var li = document.createElement("li");
     li.appendChild(document.createTextNode(itemValue));
+    li.appendChild(document.createTextNode(description));
     li.className = "list-group-item";
-
-    var deleteBtn = document.createElement("button");
-    var editBtn = document.createElement("button");
-    deleteBtn.className = "btn btn-danger btn-sm float-right delete";
-    editBtn.className = "btn btn-success btn-sm float-right edit";
-    editBtn.appendChild(document.createTextNode("Edit"));
-    deleteBtn.appendChild(document.createTextNode("X"));
-    li.appendChild(deleteBtn);
-    li.appendChild(editBtn);
+    var button = document.createElement("button");
+    button.className = "btn btn-danger btn-sm float-right delete";
+    button.textContent = "X";
+    li.appendChild(button);
     itemList.appendChild(li);
 }
 
-function removeItem(e) {
-    if(e.target.classList.contains('delete')) {
-        if(confirm('Are you sure?')) {
-            var li = e.target.parentElement;
-            itemList.removeChild(li);
+var filterItem = function(e) {
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName("li");
+    Array.from(items).forEach(item => {
+        if((item.firstChild.textContent.toLowerCase().indexOf(text) != -1)) {
+            item.style.display = "block";
+        }
+        else {
+            item.style.display = "none";
+        }
+    })
+}
+var deleteItem = function(e) {
+    if(e.target.classList.contains("delete")) {
+        if(confirm("Are you sure?")) {
+            e.target.parentNode.remove();
         }
     }
 }
+
+filter.addEventListener('keyup', filterItem);
+formInput.addEventListener('submit', addItem);
+itemList.addEventListener('click', deleteItem);
