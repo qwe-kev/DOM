ec28b1c3037af832443f6b406d2d5e7914dd48c9
qwe-kev <119116884+qwe-kev@users.noreply.github.com>
2022-12-03 16:48:03 +0530

    DOM task 12

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
+var userName = document.getElementById("name");
+var userEmail = document.getElementById("email");
+var form = document.getElementById("myForm");
+var peopleList = document.getElementById("listOfPeople");
 
-// Listen for form submit
-myForm.addEventListener('submit', onSubmit);
+function addItems(e) {
+    e.preventDefault();
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(userName.value + " " + userEmail.value));
+    peopleList.appendChild(li);
+    var editButton = document.createElement("button");
+    editButton.textContent = "EDIT";
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    peopleList.appendChild(editButton);
+    peopleList.appendChild(deleteButton);
 
-function onSubmit(e) {
-  e.preventDefault();
-  
-  if(nameInput.value === '' || emailInput.value === '') {
-    // alert('Please enter all fields');
-    msg.classList.add('error');
-    msg.innerHTML = 'Please enter all fields';
-
-    // Remove error after 3 seconds
-    setTimeout(() => msg.remove(), 3000);
-  } else {
-    // Create new list item with user
-    const li = document.createElement('li');
-
-    // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
-    // Append to ul
-    userList.appendChild(li);
-
-    // store on local storage
-    // serialize the object into string
-    let myObj = {
-        name : nameInput.value,
-        email : emailInput.value
-    } 
-    let myObj_serialized = JSON.stringify(myObj);
-    localStorage.setItem("myObj", myObj_serialized);
-
-    console.log(JSON.parse(myObj_serialized));
-    // Clear fields
-    nameInput.value = '';
-    emailInput.value = '';
-  }
+    // add to localStorage
+    let userDetails = JSON.stringify({name : userName.value, emailid : userEmail.value});
+    localStorage.setItem(`userDetails${userEmail.value}`, userDetails);
+    console.log(localStorage)
 }
-
+form.addEventListener('submit', addItems);
