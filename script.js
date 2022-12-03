779c08dfb5a2141142b4a5538b2ddce221089cb0
qwe-kev <119116884+qwe-kev@users.noreply.github.com>
2022-12-03 14:54:46 +0530

    DOM task 10

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-var itemList = document.getElementById("items");
-var formInput = document.getElementById("addForm");
-var filter = document.getElementById("filter");
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-var addItem = function(e) {
-    e.preventDefault();
-    var itemValue = document.getElementById("item").value;
-    var description = document.getElementById("item-desc").value;
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(itemValue));
-    li.appendChild(document.createTextNode(description));
-    li.className = "list-group-item";
-    var button = document.createElement("button");
-    button.className = "btn btn-danger btn-sm float-right delete";
-    button.textContent = "X";
-    li.appendChild(button);
-    itemList.appendChild(li);
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  if(nameInput.value === '' || emailInput.value === '') {
+    // alert('Please enter all fields');
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    // Remove error after 3 seconds
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    // Create new list item with user
+    const li = document.createElement('li');
+
+    // Add text node with input values
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+
+    // Add HTML
+    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+
+    // Append to ul
+    userList.appendChild(li);
+
+    // store on local storage
+    localStorage.setItem("name", nameInput.value);
+    localStorage.setItem("email", emailInput.value);
+    
+    // Clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
 }
 
-var filterItem = function(e) {
-    var text = e.target.value.toLowerCase();
-    var items = itemList.getElementsByTagName("li");
-    Array.from(items).forEach(item => {
-        if((item.firstChild.textContent.toLowerCase().indexOf(text) != -1)) {
-            item.style.display = "block";
-        }
-        else {
-            item.style.display = "none";
-        }
-    })
-}
-var deleteItem = function(e) {
-    if(e.target.classList.contains("delete")) {
-        if(confirm("Are you sure?")) {
-            e.target.parentNode.remove();
-        }
-    }
-}
-
-filter.addEventListener('keyup', filterItem);
-formInput.addEventListener('submit', addItem);
-itemList.addEventListener('click', deleteItem);
