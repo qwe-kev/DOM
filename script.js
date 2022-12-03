afd38e7c1ae87c3c6e17227d24733d3bbf3e4e66
qwe-kev <119116884+qwe-kev@users.noreply.github.com>
2022-12-03 20:40:00 +0530

    DOM task 13

--- script.js
+++ script.js
@@ -3,21 +3,44 @@ var userEmail = document.getElementById("email");
 var form = document.getElementById("myForm");
 var peopleList = document.getElementById("listOfPeople");
 
+
 function addItems(e) {
     e.preventDefault();
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(userName.value + " " + userEmail.value));
-    peopleList.appendChild(li);
     var editButton = document.createElement("button");
     editButton.textContent = "EDIT";
     var deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
     deleteButton.textContent = "X";
-    peopleList.appendChild(editButton);
-    peopleList.appendChild(deleteButton);
-
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+    peopleList.appendChild(li);
+    console.log(peopleList);
     // add to localStorage
     let userDetails = JSON.stringify({name : userName.value, emailid : userEmail.value});
-    localStorage.setItem(`userDetails${userEmail.value}`, userDetails);
-    console.log(localStorage)
+    if(JSON.parse(localStorage.getItem(`userDetails${userEmail.value}`))) {
+        var items = peopleList.getElementsByTagName("li");
+        localStorage.removeItem(`userDetails${userEmail.value}`);
+        localStorage.setItem(`userDetails${userEmail.value}`, userDetails);
+
+    }
+    else {
+        localStorage.setItem(`userDetails${userEmail.value}`, userDetails);
+    }
 }
+
+var removeItem = function(e) {
+    e.preventDefault();
+    if(e.target.classList.contains("delete")) {
+        if(confirm("Are you sure?")){
+           var li = e.target.parentElement;
+           peopleList.removeChild(li);
+        } 
+        }
+    }
+
+
+peopleList.addEventListener('click', removeItem);
+
 form.addEventListener('submit', addItems);
